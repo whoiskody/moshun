de50ae4627569dd0778462fa015ba7d342451a4d
kordaro mitchell <kody_mitchell@ymail.com>
2019-07-01 12:42:31 -0400

    template pasted

---
+++ controllers/artist.js
@@ -0,0 +1,111 @@
+// /* Step 1 import express
+//  *
+//  */
+// const express = require('express')
+
+// /* Step 2
+//  *
+//  * Import the api files from the models
+//  *
+//  * TODO: change the file path to the models file you'll need to use.
+//  * TODO: rename this from `templateApi` to something more sensible (e.g:
+//  * `shopsAPI`)
+//  *
+//  * NOTE: You may need to import more than one API to create the 
+//  * controller you need.
+//  * 
+//  */
+// const artistApi = require('../models/artist.js')
+// const albumApi = require('../models/album.js')
+
+// /* Step 3 
+//  * 
+//  * Create a new router.
+//  *
+//  * the router will "contain" all the request handlers that you define in this file.
+//  * TODO: rename this from templateRouter to something that makes sense. (e.g:
+//  * `shopRouter`)
+//  */
+// const artistRouter = express.Router()
+
+// /* Step 4
+//  * 
+//  * TODO: Put all request handlers here
+//  */
+
+// /* Step 5
+//  *
+//  * TODO: delete this handler; it's just a sample
+//  */ 
+// artistRouter.get('/', (req, res) => {
+//   artistApi.getArtists()
+//     .then((artists) => {
+//       res.render('artists/artists', {artists})
+//     })
+//     .catch((err) => {
+//       res.send(err)
+//     })
+// })
+
+// artistRouter.post('/', (req, res) => {
+//   artistApi.addArtist(req.body)
+//     .then(() => {
+//       res.redirect('/artists')
+//     })
+//     .catch((err) => {
+//       res.send(err)
+//     })
+// })
+
+// artistRouter.post('/:artistId/album', (req, res) => {
+//   req.body.artistId = req.params.artistId
+//   console.log(req.body)
+//   albumApi.addAlbum(req.body)
+//     .then(() => {
+//       res.send('Album item created')
+//     })
+// })
+
+// artistRouter.get('/new', (req, res) => {
+//   res.render('artists/newArtistForm')
+// })
+
+// artistRouter.get('/:artistId/edit', (req, res) => {
+//   artistApi.getArtist(req.params.shopId)
+//     .then((shop) => {
+//       res.render('shops/editArtistForm', {artist})
+//     })
+// })
+
+// artistRouter.get('/:artistId', (req, res) => {
+//   artistApi.getArtist(req.params.shopId)
+//     .then((artist) => {
+//       albumApi.getAlbumByArtistId(artist._id)
+//       .then((album) => {
+//         res.render('artists/singleArtist', {artist, album})
+//       })
+//   })
+// })
+
+// artistRouter.put('/:artistId', (req, res) => {
+//   artistApi.updateArtist(req.params.artistId, req.body)
+//     .then(() => {
+//       res.redirect('/artists')
+//     })
+// })
+
+// artistRouter.delete('/:artistId', (req, res) => {
+//   artistApi.deleteArtist(req.params.artistId)
+//     .then((artist) => {
+//       res.redirect('/artists')
+//     })
+// })
+
+// /* Step 6
+//  *
+//  * Export the router from the file.
+//  *
+//  */
+// module.exports = {
+//   artistRouter
+// }
---
+++ models/album.js
@@ -0,0 +1,62 @@
+// /* 
+//  * Place all functions, classes, and/or DB schemas here for a single 
+//  * model.
+//  */
+
+// /* Step 1
+//  *
+//  * TODO: import mongoose connection
+//  * NOTE: skip this if you are not using mongoose
+//  *
+//  */
+// const mongoose = require('./connection.js')
+
+// /* Step 1 alternative
+//  *
+//  * TODO: make a global variable to act as an in memory database. 
+//  * NOTE: doing this WILL NOT persist your data and you will loose
+//  * your data once you stop running your server.
+//  *
+//  */
+
+// /* Step 2
+//  *
+//  * TODO: create model schema 
+//  * NOTE: skip this if you are not using mongoose
+//  *
+//  */
+// const AlbumSchema = new mongoose.Schema({
+//   name: String,
+//   artistId: mongoose.Types.ObjectId
+//  })
+
+// /* Step 3
+//  *
+//  * TODO: create collection API
+//  * NOTE: skip this if you are not using mongoose
+//  *
+//  */
+// const AlbumCollection = mongoose.model('Album', AlbumSchema)
+
+// /* Step 4
+//  *
+//  * TODO: delete this it's just a sample
+//  *
+//  */
+// function getAlbumByArtistId(artistId) {
+//   return AlbumCollection.find({artistId: artistId})
+// }
+
+// function addAlbum(albumObject) {
+//   return AlbumCollection.create(albumObject)
+// }
+
+// /* Step 5
+//  *
+//  * TODO: export all functions from this file by adding their names as keys to this
+//  * object
+//  */
+// module.exports = {
+//   getAlbumByArtistId,
+//   addAlbum
+// }
